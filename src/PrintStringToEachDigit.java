5b198ee82b604cb1aad4b3d5d7d2705eecec5bee
ArathiRN <arathirn@gmail.com>
2024-04-30 16:58:33 -0700

    Program to print individual digits from String.

---
+++ src/PrintStringToEachDigit.java
@@ -0,0 +1,23 @@
+public class PrintStringToEachDigit {
+ /*   Using String and Character classes. Create  2 methods
+ that will print each individual  digit of a string.  You will use each
+ class to create one method.Strings length is between 2 - 100;
+    Ex String s = 597;
+    Print : Number 1 is 5
+    Print : Number 2 is 9
+    Print : Number 3 is 7
+    */
+
+    public static void main(String[] args) {
+
+        String s = "597";
+        System.out.println("Using String class:");
+        System.out.println("Using Character class:");
+       // int length = s.length();
+       // for (int i = 0; i < length; i++) {
+       //     System.out.print(s.charAt(i));
+       // }
+
+    }
+
+}
